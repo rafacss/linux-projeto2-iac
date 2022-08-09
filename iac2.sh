26cdd1dd745cea1307f55fc24e5a484a22ec899b
rafacss <rafacss@gmail.com>
2022-08-09 08:20:52 -0300

    Linux Projeto 2 IAC

--- iac2.sh
+++ iac2.sh
@@ -4,4 +4,18 @@ cd /
 # Bootcamp Linux Experience - Professor Denilson Bonatti
 # Repositório Script Bootcamp Linux Experience - Apache
 
- 
+ echo "Atualizando os pacotes do sistema"
+apt update
+echo "Instalando os novos pacotes do sistema"
+apt upgrade -y
+echo "Instalando os pacotes do servidor Apache"
+apt install apache2 unzip -y
+cd /tmp
+echo "Baixando o código do site"
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+echo "Descompactando pacote de instalação do site"
+unzip main.zip
+cd linux-site-dio
+echo"Concluindo a instalação"
+cp -R * /var/www/html
+echo"Instalação concluída"
